c14d1bec866b1740222717c6aae88a6a27410e74
wanggz <wanggz@192.168.1.8>
2016-03-29 15:46:53 +0800

    socket channel，同步，异步消息通信

--- src/main/java/org/wanggz/nio/socket/ClientSocketChannel.java
+++ src/main/java/org/wanggz/nio/socket/ClientSocketChannel.java
@@ -14,29 +14,93 @@ public class ClientSocketChannel {
 
     public static void main(String args[]) {
         ClientSocketChannel client = new ClientSocketChannel();
-        String content = "未来的域，我曾经住过~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-        client.SocketChannelBlock(content);
+        String content = "未来的域，我曾经住过~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~相约在未来的路上";
+        client.SocketChannel(content);
+    }
+
+    public void SocketChannel(String message) {
+        SocketChannel socketChannel = null;
+        ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
+        try {
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+
+            //其它socket请求参数
+            socketChannel.socket().setTcpNoDelay(true);
+            socketChannel.socket().setKeepAlive(true);
+            socketChannel.socket().setSoTimeout(5000);
+            socketChannel.connect(new InetSocketAddress("127.0.0.1", 9090));
+
+            // 连接是异步的
+            while (true) {
+                if (socketChannel.finishConnect()) {
+                    buffer.put(message.getBytes("utf-8"));
+                    buffer.flip();
+                    socketChannel.write(buffer);
+                    buffer.clear();
+                    break;
+                } else {
+                    Thread.sleep(100);
+                }
+            }
+            int readTime = 0;
+            while (true) {
+                readTime++;
+                if (socketChannel.finishConnect() && socketChannel.read(buffer) > 0) {
+                    buffer.flip();
+                    while (buffer.hasRemaining()) {
+                        System.out.print((char) buffer.get());
+                    }
+                    buffer.clear();
+                    break;
+                } else {
+                    Thread.sleep(100);
+                }
+            }
+
+            System.out.println("\n读了多少次, " + readTime);
+            Thread.sleep(5000);
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socketChannel != null) {
+                    socketChannel.close();
+                    socketChannel = null;
+                }
+            } catch (Throwable e) {
+
+            }
+        }
     }
 
     public void SocketChannelBlock(String message) {
         SocketChannel socketChannel = null;
+        ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
         try {
             socketChannel = SocketChannel.open();
             socketChannel.configureBlocking(true);
+
+            //其它socket请求参数
+            socketChannel.socket().setTcpNoDelay(true);
+            socketChannel.socket().setKeepAlive(true);
+            socketChannel.socket().setSoTimeout(5000);
+
             socketChannel.connect(new InetSocketAddress("127.0.0.1", 9090));
 
             if (socketChannel.finishConnect()) {
-                ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
                 buffer.put(message.getBytes("utf-8"));
                 buffer.flip();
                 socketChannel.write(buffer);
-            }
+                buffer.clear();
 
-            ByteBuffer dst = ByteBuffer.allocateDirect(2048);
-            socketChannel.read(dst);
-            dst.flip();
-            while (dst.hasRemaining()) {
-                System.out.print((char) dst.get());
+                socketChannel.read(buffer);
+                buffer.flip();
+                while (buffer.hasRemaining()) {
+                    System.out.print((char) buffer.get());
+                }
+                buffer.clear();
             }
         } catch (Throwable e) {
             e.printStackTrace();
--- src/main/java/org/wanggz/nio/socket/ServiceSocketChannel.java
+++ src/main/java/org/wanggz/nio/socket/ServiceSocketChannel.java
@@ -10,33 +10,45 @@ import java.nio.channels.SocketChannel;
  */
 public class ServiceSocketChannel {
 
-    public ServiceSocketChannel() {
-    }
-
-    public static void serverSocket(int port) {
-        ServerSocketChannel serverSocket = null;
+    public static void ServiceSocket(int port) {
         try {
-            serverSocket = ServerSocketChannel.open();
-            serverSocket.configureBlocking(true);
+            final ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            serverSocket.configureBlocking(false);
+
+            serverSocket.socket().setSoTimeout(5000);
             serverSocket.socket().bind(new InetSocketAddress(port));
 
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (serverSocket != null) {
+                            serverSocket.close();
+                        }
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
+                }
+            }));
+
             //服务提供
-            int total = 0, index = 0;
+            int total = 0, index = 0, capacity = 0;
             SocketChannel socketChannel = null;
             ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
-            byte[] totalBuffer = new byte[12288];
             while (true) {
                 total++;
+                index = 0;
                 try {
                     if ((socketChannel = serverSocket.accept()) != null) {
-
-                        socketChannel.read(buffer);
+                        //读取最大字节数
+                        capacity = socketChannel.read(buffer);
                         buffer.flip();
 
+                        byte[] totalBuffer = new byte[2048];
                         while (buffer.hasRemaining()) {
                             totalBuffer[index++] = buffer.get();
                         }
-                        System.out.println(total + new String(totalBuffer, "utf-8"));
+                        System.out.println(total + "," + new String(totalBuffer, "utf-8"));
 
                         buffer.clear();
                         buffer.put((total + ", service socket rsp : " + total).getBytes("utf-8"));
@@ -51,20 +63,65 @@ public class ServiceSocketChannel {
 
         } catch (Throwable e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (serverSocket != null) {
-                    serverSocket.close();
-                    serverSocket = null;
+        }
+    }
+
+    public static void serverSocketBlocking(int port) {
+        try {
+            final ServerSocketChannel serverSocket = ServerSocketChannel.open();
+            serverSocket.configureBlocking(true);
+
+            serverSocket.socket().setSoTimeout(5000);
+            serverSocket.socket().bind(new InetSocketAddress(port));
+
+            Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        if (serverSocket != null) {
+                            serverSocket.close();
+                        }
+                    } catch (Throwable e) {
+                        e.printStackTrace();
+                    }
                 }
-            } catch (Throwable e) {
-                e.printStackTrace();
+            }));
+
+            //服务提供
+            int total = 0, index = 0;
+            SocketChannel socketChannel = null;
+            ByteBuffer buffer = ByteBuffer.allocateDirect(2048);
+            while (true) {
+                total++;
+                index = 0;
+                try {
+                    socketChannel = serverSocket.accept();
+                    socketChannel.read(buffer);
+                    buffer.flip();
+
+                    byte[] totalBuffer = new byte[2048];
+                    while (buffer.hasRemaining()) {
+                        totalBuffer[index++] = buffer.get();
+                    }
+                    System.out.println(total + "," + new String(totalBuffer, "utf-8"));
+
+                    buffer.clear();
+                    buffer.put((total + ", service socket rsp : " + total).getBytes("utf-8"));
+                    buffer.flip();
+                    socketChannel.write(buffer);
+                    buffer.clear();
+                } catch (Throwable e) {
+                    e.printStackTrace();
+                }
             }
+
+        } catch (Throwable e) {
+            e.printStackTrace();
         }
     }
 
     public static void main(String args[]) {
         ServiceSocketChannel server = new ServiceSocketChannel();
-        server.serverSocket(9090);
+        server.serverSocketBlocking(9090);
     }
 }
